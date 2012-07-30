1950e2e7340efff36e151d27eec933aa4df2673f
TheVasari <thevasari@gmail.com>
2012-07-30 21:11:52 +1000

    Created Android project and started the UI for the login screen.

---
+++ Lacuna Expanse/src/com/lacunaexpanse/LacunaAndroidClient/Login.java
@@ -0,0 +1,22 @@
+package com.lacunaexpanse.LacunaAndroidClient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Login extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        //I need help with this...
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.serverOptions, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource(R.layout.selectServer);
+        Spinner spinner = (Spinner) findViewById(R.id.selectServer);
+        spinner.setAdapter(adapter);
+        		
+    }
+}
