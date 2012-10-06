a84a1764be97756a7bde677a77fb214d187a8ce1
TheVasari <NathanMcCallum@hotmail.com>
2012-10-07 08:25:28 +1000

    I'M BACK EVERYONE! Ok, I've added the update reminder in the login screen.

--- Lacuna Expanse/src/com/lacunaexpanse/LacunaAndroidClient/Login.java
+++ Lacuna Expanse/src/com/lacunaexpanse/LacunaAndroidClient/Login.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends Activity {
@@ -19,9 +20,28 @@ public class Login extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		final int APP_VERSION = 005;
+		final String CURRENT_ONLINE_VERSION_JSON = Library.sendServerRequest("http://www.lacuna-android-client.webs.com/current_version/current_version.txt");
+		TextView versionInformation = (TextView) findViewById(R.id.versionInformation);
+		
+		int CURRENT_ONLINE_VERSION = 0;
+		try {
+			JSONObject jObject = new JSONObject(CURRENT_ONLINE_VERSION_JSON);
+			CURRENT_ONLINE_VERSION = jObject.getInt("version_number");
+		}
+		catch (JSONException e) {
+			versionInformation.setText("Couldn't determine weather Lacuna Expanse needs updating or not: " + e.toString());
+		}
+			
+		if (APP_VERSION == CURRENT_ONLINE_VERSION) {
+			versionInformation.setText("Your version of Lacuna Expanse is up-to-date!");
+		}
+		else {
+			versionInformation.setText("Your version of Lacuna Expanse is out of date.\nPlease go to www.lacuna-android-client.webs.com and update the application.");
+		}
 
 		Button loginButton = (Button) findViewById(R.id.loginButton);
-
 		loginButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				ProgressDialog loadingDialog = new ProgressDialog(Login.this);
@@ -42,11 +62,11 @@ public class Login extends Activity {
 
 				if (empireName.length() <= 0) {
 					LOADING_DIALOG.dismiss();
-					Toast.makeText(Login.this,"Please enter your empire name",Toast.LENGTH_SHORT).show();
+					Toast.makeText(Login.this,"Please enter your empire name.",Toast.LENGTH_SHORT).show();
 				}
 				else if (empirePassword.length() <= 0) {
 					LOADING_DIALOG.dismiss();
-					Toast.makeText(Login.this,"Please enter your empire password",Toast.LENGTH_SHORT).show();
+					Toast.makeText(Login.this,"Please enter your empire password.",Toast.LENGTH_SHORT).show();
 				}
 				else {
 					// Set selectedServer and apiKey based on selected item in Spinner. Server defaults to US1.
@@ -54,7 +74,7 @@ public class Login extends Activity {
 					String apiKey = null;
 					if (indexValue == 0) {
 						LOADING_DIALOG.dismiss();
-						Toast.makeText(Login.this,"Please select a server",Toast.LENGTH_SHORT).show();
+						Toast.makeText(Login.this,"Please select a server.",Toast.LENGTH_SHORT).show();
 					}
 					else if (indexValue == 1) {
 						selectedServer = "us1";
